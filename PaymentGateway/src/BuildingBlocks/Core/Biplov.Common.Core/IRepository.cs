babaf87bd12d00ba3ed7f393f5ce6211de3e0a84
Biplov KC <biplov.cybercop@gmail.com>
2020-12-10 17:48:50 +0100

    Added common interfaces

---
+++ PaymentGateway/src/BuildingBlocks/Core/Biplov.Common.Core/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Biplov.Common.Core
+{
+    /// <summary>
+    /// Every aggregate root can have its own repository for data persistence and data read.
+    /// Further functionality can be added by the interfaces that inherit this interface
+    /// </summary>
+    /// <typeparam name="T">Aggregate root</typeparam>
+    public interface IRepository<T> where T: Entity
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
---
+++ PaymentGateway/src/BuildingBlocks/Core/Biplov.Common.Core/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Biplov.Common.Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<Result> SaveEntitiesAsync(CancellationToken cancellationToken = default);
+    }
+}
