27274334ff84c12d5c6567b78067258951bfcc2e
Biplov KC <biplov.cybercop@gmail.com>
2020-12-10 17:50:56 +0100

    Added extension classes

---
+++ PaymentGateway/src/BuildingBlocks/Core/Biplov.Common.Core/Extensions/LinqExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Biplov.Common.Core.Extensions
+{
+    public static class LinqExtensions
+    {
+        /// <summary>
+        /// Checks if any given sequence is empty or not
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sequence"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> OrEmpty<T>(this IEnumerable<T> sequence)
+        {
+            return sequence ?? Enumerable.Empty<T>();
+        }
+    }
+}
