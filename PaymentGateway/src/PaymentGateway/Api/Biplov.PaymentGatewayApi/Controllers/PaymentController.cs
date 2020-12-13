c27fb8be481dcbb549fe69dbe0e6ea5d10f9000c
Biplov KC <biplov.cybercop@gmail.com>
2020-12-13 08:05:30 +0100

    [WIP] PaymentGateway api layer

---
+++ PaymentGateway/src/PaymentGateway/Api/Biplov.PaymentGatewayApi/Controllers/PaymentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Biplov.PaymentGatewayApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        
+        private readonly ILogger<PaymentController> _logger;
+
+        public PaymentController(ILogger<PaymentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
