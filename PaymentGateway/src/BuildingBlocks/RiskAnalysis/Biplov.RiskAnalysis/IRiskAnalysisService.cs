c5fc3916c4132c04f28dfc18dda17ebeba75504e
Biplov KC <biplov.cybercop@gmail.com>
2020-12-13 23:32:19 +0100

    BuildingBlock : riskanalysis added to handle fake ip risk analysis

---
+++ PaymentGateway/src/BuildingBlocks/RiskAnalysis/Biplov.RiskAnalysis/IRiskAnalysisService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Biplov.Common.Core;
+
+namespace Biplov.RiskAnalysis
+{
+    /// <summary>
+    /// Risk analysis service
+    /// </summary>
+    public interface IRiskAnalysisService
+    {
+        /// <summary>
+        /// Basic risk analysis, can be modified or extended as per requirement
+        /// </summary>
+        /// <param name="ipAddress">Request origin ip</param>
+        /// <returns>Risk assessment result</returns>
+        Task<Result<RiskAssessmentResult>> GetRiskAnalysis(IPAddress ipAddress);
+    }
+}
---
+++ PaymentGateway/src/BuildingBlocks/RiskAnalysis/Biplov.RiskAnalysis/RiskAssessmentResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Biplov.RiskAnalysis
+{
+    public class RiskAssessmentResult
+    {
+        public bool IsRisky { get; }
+
+        public string Reason { get; }
+
+        public RiskAssessmentResult(bool isRisky, string reason)
+        {
+            IsRisky = isRisky;
+            Reason = reason;
+        }
+    }
+}
