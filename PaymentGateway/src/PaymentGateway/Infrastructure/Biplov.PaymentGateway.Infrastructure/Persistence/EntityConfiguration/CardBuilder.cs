332cccc9b6c838bb4bfcea407e94c4e09103dbfe
Biplov KC <biplov.cybercop@gmail.com>
2020-12-13 23:33:59 +0100

    entity builders

---
+++ PaymentGateway/src/PaymentGateway/Infrastructure/Biplov.PaymentGateway.Infrastructure/Persistence/EntityConfiguration/CardBuilder.cs
@@ -0,0 +1,35 @@
+﻿using Biplov.PaymentGateway.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Biplov.PaymentGateway.Infrastructure.Persistence.EntityConfiguration
+{
+    public class CardBuilder : IEntityTypeConfiguration<Card>
+    {
+        public void Configure(EntityTypeBuilder<Card> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.HasIndex(x => x.CardToken)
+                .IsUnique();
+
+            builder.Property(x => x.CardToken)
+                .HasMaxLength(40);
+
+            builder.Ignore(x => x.DomainEvents);
+
+            builder.Property(x => x.Number)
+                .IsRequired()
+                .HasMaxLength(19);
+
+            builder.Property(x => x.MaskedCardNumber)
+                .HasMaxLength(19);
+
+            builder.Property(x => x.ExpiryMonth)
+                .IsRequired();
+
+            builder.Property(x => x.Cvv)
+                .HasMaxLength(4);
+        }
+    }
+}
--- PaymentGateway/src/PaymentGateway/Infrastructure/Biplov.PaymentGateway.Infrastructure/Persistence/EntityConfiguration/PaymentBuilder.cs
+++ PaymentGateway/src/PaymentGateway/Infrastructure/Biplov.PaymentGateway.Infrastructure/Persistence/EntityConfiguration/PaymentBuilder.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Biplov.PaymentGateway.Domain.Entities;
+﻿using Biplov.PaymentGateway.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -9,7 +8,111 @@ namespace Biplov.PaymentGateway.Infrastructure.Persistence.EntityConfiguration
     {
         public void Configure(EntityTypeBuilder<Payment> builder)
         {
-            throw new NotImplementedException();
+            builder.ToTable("Payments");
+            builder.HasKey(x => x.Id);
+            
+            builder.Ignore(x => x.DomainEvents);
+
+            builder.HasIndex(x => x.MerchantId)
+                .IsUnique();
+
+            builder.Property(x => x.Currency)
+                .HasMaxLength(3);
+
+            builder.OwnsOne(x => x.Source, r =>
+            {
+                r.Property(x => x.CardToken)
+                    .HasColumnName("CardToken")
+                    .HasMaxLength(45);
+
+                r.Property(x => x.Cvv)
+                    .HasColumnName("Cvv")
+                    .HasMaxLength(4);
+            });
+
+            var paymentSourceNavigation = builder.Metadata.FindNavigation(nameof(Payment.Source));
+            paymentSourceNavigation.SetPropertyAccessMode(PropertyAccessMode.Field);
+
+            builder.OwnsOne(x => x.BillingAddress, r =>
+            {
+                r.Property(x => x.AddressLine1)
+                    .HasColumnName("AddressLine1")
+                    .HasMaxLength(100);
+
+                r.Property(x => x.AddressLine2)
+                    .HasColumnName("AddressLine2")
+                    .HasMaxLength(100);
+
+                r.Property(x => x.ZipCode)
+                    .HasColumnName("ZipCode")
+                    .HasMaxLength(20);
+
+                r.Property(x => x.State)
+                    .HasColumnName("State")
+                    .HasMaxLength(100);
+
+                r.Property(x => x.City)
+                    .HasColumnName("City")
+                    .HasMaxLength(100);
+
+                r.Property(x => x.Country)
+                    .HasColumnName("Country")
+                    .HasMaxLength(100);
+            });
+
+            var billingAddressNavigation = builder.Metadata.FindNavigation(nameof(Payment.BillingAddress));
+            billingAddressNavigation.SetPropertyAccessMode(PropertyAccessMode.Field);
+
+            builder.OwnsOne(x => x.Recipient, r =>
+            {
+                r.Property(x => x.AccountNumber)
+                    .HasColumnName("RecipientAccountNumber")
+                    .HasMaxLength(20);
+
+                r.Property(x => x.DateOfBirth)
+                    .HasColumnName("RecipientDateOfBirth");
+
+                r.Property(x => x.FirstName)
+                    .HasColumnName("RecipientFirstName")
+                    .HasMaxLength(250);
+
+                r.Property(x => x.LastName)
+                    .HasColumnName("RecipientLastName")
+                    .HasMaxLength(250);
+
+                r.Property(x => x.ZipCode)
+                    .HasColumnName("RecipientZipCode")
+                    .HasMaxLength(20);
+            });
+
+            var paymentRecipient = builder.Metadata.FindNavigation(nameof(Payment.Recipient));
+            paymentRecipient.SetPropertyAccessMode(PropertyAccessMode.Field);
+
+            builder.OwnsOne(x => x.MerchantNotification, r =>
+            {
+                r.Property(x => x.SuccessUrl)
+                    .HasColumnName("SuccessWebHookUrl");
+
+                r.Property(x => x.ErrorUrl)
+                    .HasColumnName("ErrorWebHookUrl");
+            });
+
+            var merchantNotificationNavigation = builder.Metadata.FindNavigation(nameof(Payment.MerchantNotification));
+            merchantNotificationNavigation.SetPropertyAccessMode(PropertyAccessMode.Field);
+
+            builder.OwnsMany<MetaData>(x => x.MetaData, r =>
+            {
+                r.ToTable("PaymentMetaData");
+
+                r.Property(x => x.Key)
+                    .HasColumnName("Key");
+
+                r.Property(x => x.Value)
+                    .HasColumnName("Value");
+            });
+
+            var metaDataNavigation = builder.Metadata.FindNavigation(nameof(Payment.MetaData));
+            metaDataNavigation.SetField("_metaData");
         }
     }
 }
