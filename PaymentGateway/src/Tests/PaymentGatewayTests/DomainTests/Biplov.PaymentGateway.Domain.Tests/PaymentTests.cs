12c3a7d07249b523ff899d48ab7abd5c57591f5e
Biplov KC <biplov.cybercop@gmail.com>
2020-12-15 08:55:45 +0100

    Added payment gateway domain tests

---
+++ PaymentGateway/src/Tests/PaymentGatewayTests/DomainTests/Biplov.PaymentGateway.Domain.Tests/PaymentTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using AutoFixture.Xunit2;
+
+using Biplov.PaymentGateway.Domain.Entities;
+using Biplov.PaymentGateway.Domain.Enum;
+using Shouldly;
+using Xunit;
+
+namespace Biplov.PaymentGateway.Domain.Tests
+{
+    public class PaymentTests
+    {
+        [Theory, AutoData]
+        public void InitiatePayment_SetsPaymentStatus_ToInProcess(Guid merchantId, decimal amount, string reference, string description)
+        {
+            // Arrange
+            var payment = new Payment(merchantId, "EUR", amount, reference, "", description);
+            payment.SetCardPaymentSource("cardtok_abcasdresadfeadfin123", "251");
+            // Act
+            payment.InitiatePayment();
+
+            // Assert
+            payment.Status.ShouldBe(PaymentStatus.InProcess);
+        }
+
+        [Theory, AutoData]
+        public void InitiatePayment_SetsPaymentStatus_RaisesDomainEvent(Guid merchantId, decimal amount, string reference, string description)
+        {
+            // Arrange
+            var payment = new Payment(merchantId, "EUR", amount, reference, "", description);
+            payment.SetCardPaymentSource("cardtok_abcasdresadfeadfin123", "251");
+            // Act
+            payment.InitiatePayment();
+
+            // Assert
+            payment.DomainEvents.Count.ShouldBe(1);
+        }
+    }
+}
