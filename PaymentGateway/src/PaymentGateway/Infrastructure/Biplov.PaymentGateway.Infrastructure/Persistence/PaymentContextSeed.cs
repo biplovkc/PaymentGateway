11d6818b247cd10ead55b904be0d30784cdf9d76
Biplov KC <biplov.cybercop@gmail.com>
2020-12-18 12:09:56 +0100

    Apply retry logic for paymentContext when the db creation fails
    Db creation might fail for payment context because the sql container might not have been created while paymentgateway tries to apply migration. So retry logic have been applied for migrating db

---
+++ PaymentGateway/src/PaymentGateway/Infrastructure/Biplov.PaymentGateway.Infrastructure/Persistence/PaymentContextSeed.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Biplov.PaymentGateway.Infrastructure.Persistence
+{
+    public class PaymentContextSeed
+    {
+        public void Seed(PaymentContext context, ILogger<PaymentContextSeed> logger, int? retry = 5)
+        {
+            var retryForAvailability = retry ?? 0;
+            try
+            {
+                context.Database.EnsureCreated();
+            }
+            catch (Exception e)
+            {
+                if (retryForAvailability < 10)
+                {
+                    retryForAvailability++;
+                    logger.LogError(e, "EXCEPTION ERROR while migrating {DbContextName}", nameof(PaymentContext));
+                    Seed(context, logger, retryForAvailability);
+                }
+            }
+        }
+    }
+}
